5fd41103c66a6640536f2bdcb10b9861c3ef14f9
Webforce3 <denis.nerborac@wf3.fr>
2016-05-03 10:38:49 +0200

    FIX error variable

--- ajax-check-login.php
+++ ajax-check-login.php
@@ -10,7 +10,7 @@ if (empty($login) || !filter_var($login, FILTER_VALIDATE_EMAIL)) {
 	$error = 'Veuillez renseigner un email valide';
 }
 
-if (empty($errors)) {
+if (empty($error)) {
 
 	// On vérifie que le login/email est pas deja pris
 	$query = $db->prepare('SELECT * FROM user WHERE login = :login');
